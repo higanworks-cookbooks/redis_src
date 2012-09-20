0112b1f96e25cce52e29ad3586d4a8b8851e3650
sawanoboly <sawanoboriyu@higanworks.com>
2012-09-20 15:28:45 +0900

    update redis. vers 2.4.16 -> 2.4.17

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
-default[:redis][:version] = "2.4.16"
+default[:redis][:version] = "2.4.17"
 
 default[:redis][:src_cachedir] = "#{Chef::Config[:file_cache_path]}/redis"
--- metadata.rb
+++ metadata.rb
@@ -3,4 +3,4 @@ maintainer_email "sawanoboriyu@higanworks.com"
 license          "MIT"
 description      "Installs/Configures redis_src"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.2"
